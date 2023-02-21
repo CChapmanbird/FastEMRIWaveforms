731e2d84fb7c1fe293a21a3f6a942dc2a50ab328
Lorenzo Speri <lorenzo.speri@gmail.com>
2023-02-21 12:16:03 +0100

    fixed gpu implementation

--- src/gpuAAK.cu
+++ src/gpuAAK.cu
@@ -66,7 +66,13 @@ void d_RotCoeff(double* rot, double* n, double* L, double* S, double* nxL, doubl
 }
 
 #define  NUM_PARS 8
-#define MAX_SPLINE_POINTS 200
+
+#ifdef __CUDACC__
+#define MAX_SPLINE_POINTS 160
+#else
+#define MAX_SPLINE_POINTS 10000
+#endif
+
 CUDA_KERNEL
 void make_waveform(cmplx *waveform,
               double* interp_array,
@@ -107,10 +113,10 @@ void make_waveform(cmplx *waveform,
       end = 4 * NUM_PARS;
       increment = 1;
 
-      #ifdef __USE_OMP__
+#ifdef __USE_OMP__
       #pragma omp parallel for
-      #endif  // __USE_OMP__
-      #endif // __CUDACC__
+#endif  // __USE_OMP__
+#endif // __CUDACC__
 
        // prepare interpolants
       // 8 parameters, 4 coefficient values for each parameter
@@ -188,10 +194,10 @@ void make_waveform(cmplx *waveform,
       end = end_ind;
       increment = 1;
 
-      #ifdef __USE_OMP__
+#ifdef __USE_OMP__
       #pragma omp parallel for
-      #endif
-      #endif
+#endif  // __USE_OMP__
+#endif // __CUDACC__
       for (int i = start; i < end; i += increment)
       {
 
