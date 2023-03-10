eb2bc74cef851c63fa5f30528f86b4a82abb1a8e
lorenzsp <lorenzo.speri@gmail.com>
2023-03-10 13:19:12 +0100

    separatrix for Kerr Eq using Steffenson solver gsl

--- few/tests/test_traj.py
+++ few/tests/test_traj.py
@@ -29,6 +29,42 @@ except (ModuleNotFoundError, ImportError) as e:
 class ModuleTest(unittest.TestCase):
     def test_trajectory(self):
 
+        # initialize trajectory class
+        traj = EMRIInspiral(func="pn5")
+
+        # set initial parameters
+        M = 1e5
+        mu = 1e1
+        np.random.seed(42)
+        for i in range(50):
+            p0 = np.random.uniform(10.0,15)
+            e0 = np.random.uniform(0.0, 1.0)
+            a = np.random.uniform(0.0, 1.0)
+            Y0 = np.random.uniform(-1.0, 1.0)
+
+            # do not want to be too close to polar
+            if np.abs(Y0) < 1e-2:
+                Y0 = np.sign(Y0) * 1e-2
+
+            # run trajectory
+            #print("start", a, p0, e0, Y0)
+            t, p, e, x, Phi_phi, Phi_theta, Phi_r = traj(M, mu, a, p0, e0, Y0, **insp_kw)
+
+
+    def test_trajectory_SchwarzEccFlux(self):
+        # initialize trajectory class
+        traj = EMRIInspiral(func="SchwarzEccFlux")
+
+        # set initial parameters
+        M = 1e5
+        mu = 1e1
+        p0 = 10.0
+        e0 = 0.7
+
+        # run trajectory
+        t, p, e, x, Phi_phi, Phi_theta, Phi_r = traj(M, mu, 0.0, p0, e0, 1.0)
+
+    def test_trajectory_KerrEccentricEquatorial(self):
 
         err = 1e-10
         
