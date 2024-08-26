456080cc9b9741102df26192aa66ba0f010d654d
Christian Chapman-Bird <c.chapman-bird.1@research.gla.ac.uk>
2024-08-26 17:15:49 +0100

    mamba to conda

--- scripts/build_env.sh
+++ scripts/build_env.sh
@@ -47,7 +47,7 @@ conda activate test_gpu_install_env
 
 if [[ $1 == "ubuntu-latest" ]]
 then
-    mamba install -c conda-forge cupy cuda-nvcc cuda-libraries-dev cuda-version=$2} -y
+    conda install -c conda-forge cupy cuda-nvcc cuda-libraries-dev cuda-version=$2} -y
     export CUDAHOME=$CONDA_PREFIX
     ln -s $CONDA_PREFIX/lib $CONDA_PREFIX/lib64
     cp -r $CONDA_PREFIX/targets/$(ls $CONDA_PREFIX/targets/ | head -1)/include/* $CONDA_PREFIX/include/ 
